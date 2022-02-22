cfb48327faf5305a366cb7d333658daa33ddebbd
Agrawal, Ritika <Ritika.Agrawal@anz.com>
2022-02-22 07:26:32 +0530

    module3

--- vehicle.go
+++ vehicle.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"fmt"
 )
 
 type vehicle interface{
@@ -86,6 +87,17 @@ func main() {
 	generateRating()
 	
 	// Print ratings for the different vehicles
+	for _, veh := range inventory {
+		switch v := veh.type() {
+		case car:
+			v.carDetails()
+		case bike:
+			v.bikeDetails()
+		case truck:
+			v.truckDetails()
+		default:
+			fmt.Printf("Are you sure this Vehicle Type exists")
+	}
 }
 
 func readJSONFile() Values {
@@ -137,4 +149,33 @@ func generateRating() {
 		}
 		vehicleResult[v.Name] = vehResult
 	}
+}
+
+func showRating(model string) {
+	ratingFound := false
+	for m, r := range vehicleResult {
+		if m == model {
+			fmt.Printf("Total Ratings:%v\tPositive:%v\tNegative:%v\tNeutral:%v", r.feedbackTotal, r.feedbackPositive, r.feedbackNegative, r.feedbackNeutral)
+			ratingFound = true
+		}
+	}
+	if !ratingFound {
+		fmt.Printf("No rating for this vehicle")
+	}
+} 
+
+//Defining carDetails() method with type *car
+func (c *car) carDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Car" , c.make, c.model)
+	showRating(c.Model)
+}
+
+func (b *bike) bikeDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Bike" , b.make, b.model)
+	showRating(b.Model)
+}
+
+func (t *truck) truckDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Truck" , t.make, t.model)
+	showRating(t.Model)
 }
